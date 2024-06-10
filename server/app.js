34a5299135dde0ad88a2154422f94bf8ddb822d7
Abner Nieves <nieves.abner@gmail.com>
2024-06-10 17:37:32 -0400

    first day tasks

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const PORT = 5005;
+app.use(cors({origin: `http://127.0.0.1:${PORT}`}))
+
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,7 +23,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
@@ -29,7 +30,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.sendFile(__dirname + "/cohorts.json");
+});
 
+app.get("/api/students", (req, res) => {
+  res.sendFile(__dirname + "/students.json");
+});
 // START SERVER
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
