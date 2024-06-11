aab060181ae8d5c704e3f75992d65db36de1db79
Abner Nieves <nieves.abner@gmail.com>
2024-06-11 18:07:05 -0400

    added student and cohort models and schema 🫡

--- server/models/Cohorts.model.js
+++ server/models/Cohorts.model.js
@@ -2,17 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const cohortSchema = new Schema({
-  cohortSlug: String,
-  cohortName: String,
-  program: String,
-  format: String,
-  campus: String,
-  startDate: Date,
-  endDate: Date,
-  inProgress: Boolean,
-  programManager: String,
-  leadTeacher: String,
-  totalHours: Number,
+  cohortSlug: { type: String, required: true },
+  cohortName: { type: String, required: true },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  format: { type: String, enum: ["Full Time", "Part Time"] },
+  campus: {
+    type: String,
+    enum: [
+      "Madrid",
+      "Barcelona",
+      "Miami",
+      "Paris",
+      "Berlin",
+      "Amsterdam",
+      "Lisbon",
+      "Remote",
+    ],
+  },
+  startDate: { type: Date, default: Date.now },
+  endDate: { type: Date, default: Date.end },
+  inProgress: { type: Boolean, default: false },
+  programManager: { type: String, required: true },
+  leadTeacher: { type: String, required: true },
+  totalHours: { type: Number, min: 0, default: 360 },
 });
 
 const Cohort = mongoose.model("Cohort", cohortSchema);
